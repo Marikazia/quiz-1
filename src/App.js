b0bb538541c4e81180373b8def6cc3ac3d249213
Mariana Machohan <debrisofme9@gmail.com>
2022-10-12 10:57:05 +0200

    some fixes

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function Game({ step, question, onClickVariant }) {
 	return (
 		<>
 			<div className="progress">
-				<div style={{ width: `${percentage}` }} className="progress__inner"></div>
+				<div style={{ width: `${percentage}%` }} className="progress__inner"></div>
 			</div>
 			<h1>{question.title}</h1>
       <ul>
